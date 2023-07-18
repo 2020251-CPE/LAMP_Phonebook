0ec58817e473d4d0f9b626d1a80f3e3e46448d25
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-07-18 15:18:01 +0800

    created "/register" route for register page

--- Phonebook8.1.0/index.php
+++ Phonebook8.1.0/index.php
@@ -16,6 +16,9 @@ switch ($request) {
     case '/login':
         require __DIR__ . $viewDir . 'login.php';
         break;
+    case '/register':
+        require __DIR__ . $viewDir . 'register.php';
+        break;
     default:
         http_response_code(404);
         require __DIR__ . $viewDir . '404.php';
