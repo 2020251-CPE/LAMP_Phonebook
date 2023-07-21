08b316694a3af643ce485318be1fdff6d1d5fac8
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-07-21 13:03:13 +0800

    Added try catch + Changed email column Name

--- Phonebook8.1.0/api/fetch_contacts.php
+++ Phonebook8.1.0/api/fetch_contacts.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 require_once 'DB_connection.php';
-
+try{
 // Fetch contacts from the database
 $sql = "SELECT phonebook.id, phonebook.name, phonebook.middleName, phonebook.lastName, 
 	phonebook.number, phonebook.email, phonebook.address, 
-	phonebook.notes, accounts.email
+	phonebook.notes, accounts.userEmail
 FROM phonebook
 INNER JOIN accounts ON phonebook.ownerID=accounts.id
-WHERE accounts.email = '".$_SESSION["user"]."';
+WHERE accounts.userEmail = '".$_SESSION["user"]."';
 ";
 $result = mysqli_query($con, $sql);
 $contacts = array();
@@ -19,4 +19,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 // Return contacts as JSON
 header('Content-Type: application/json');
 echo json_encode($contacts);
+}catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
+}
 ?>
--- Phonebook8.1.0/api/save_contact.php
+++ Phonebook8.1.0/api/save_contact.php
@@ -1,6 +1,7 @@
 <?php
+session_start();
 require_once 'DB_connection.php';
-
+try{
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name =  $_POST['name'];
     $mname = $_POST['mname'];
@@ -10,16 +11,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $address = $_POST['address'];
     $notes = $_POST['notes'];
 
-    $sql = "INSERT INTO phonebook (name, middleName, lastName,  number, email, address, notes) 
-            VALUES ('$name', '$mname', '$lname','$number', '$email', '$address', '$notes')";
+    $sql = "INSERT INTO phonebook (name, middleName, lastName,  number, email, address, notes, ownerID) 
+            VALUES ('$name', '$mname', '$lname','$number', '$email', '$address', '$notes',
+            (SELECT id FROM accounts WHERE userEmail = '".$_SESSION["user"]."'));";
     if (mysqli_query($con, $sql)) {
         $response = array('success' => true);
     } else {
         $response = array('success' => false, 'error' => mysqli_error($con));
     }
-
     echo json_encode($response);
 } else {
     echo 'Invalid request';
+}
+    
+}catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
 }
 ?>
