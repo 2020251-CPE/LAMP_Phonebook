2fbd2ef4776d9c061951b0b03eae9115d8b82335
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-07-21 13:04:16 +0800

    Added some paths for when Dev Checks the solo files

--- Phonebook8.1.0/index.php
+++ Phonebook8.1.0/index.php
@@ -7,15 +7,18 @@ switch ($request) {
     case '':
     case '/':
     case '/index.php':
+    case '/views/home.php':
         require __DIR__ . $viewDir . 'home.php';
         break;
-        
+    case '/views/bookphone.php':
     case '/phonebook':
         require __DIR__ . $viewDir . 'bookphone.php';
         break;
+    case '/views/login.php':
     case '/login':
         require __DIR__ . $viewDir . 'login.php';
         break;
+    case '':
     case '/register':
         require __DIR__ . $viewDir . 'register.php';
         break;
